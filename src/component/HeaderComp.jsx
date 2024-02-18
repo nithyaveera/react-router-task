c051329fb1b90d4281896ccc9a30151a6ee32197
nithyaveera <nithyaveeramani2003@gmail.com>
2024-02-18 23:24:11 +0530

    update

--- src/component/HeaderComp.jsx
+++ src/component/HeaderComp.jsx
@@ -2,16 +2,25 @@ import React from 'react';
 
 const HeaderComp = () => {
     return (
-        <div className='d-flex justify-content-between p-2' > 
-            <img src="https://www.guvi.in/blog/wp-content/uploads/2022/04/Guvi-blog-logo.png" alt="" srcset="" />
-            <div className='d-flex justify-content-between p-3'>
-                <span > COURSES</span>
-                <span style={{ paddingLeft: '6vw' }}>LIVE CLASSES</span>
-                <span style={{ paddingLeft: '6vw' }}>PRACTICE</span>
-                <span style={{ paddingLeft: '6vw' }}>RESOURCE</span>
-                <span style={{ paddingLeft: '6vw' }}>OUR PRODUCTS</span>
-            
+        <div className='row p-3' > 
+            <div className='col-md-2'>
+                <img src="https://www.guvi.in/blog/wp-content/uploads/2022/04/Guvi-blog-logo.png" className='img-fluid ' alt="" srcset="" />
             </div>
+            <div className='col-md-2 p-3'>
+                <span > COURSES</span>
+            </div>
+            <div className='col-md-2 p-3'>
+                <span>LIVE CLASSES</span>
+            </div>
+            <div className='col-md-2 p-3'>
+                <span >PRACTICE</span>
+            </div>
+            <div className='col-md-2 p-3'>
+                <span >RESOURCE</span>
+            </div>
+            <div className='col-md-2 p-3'>
+                <span>OUR PRODUCTS</span>
+          </div>
         </div>
     );
 };
